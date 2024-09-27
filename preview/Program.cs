6eb0f14134fafa630c83b58a9e92117d594f5430
Marcel Kwiatkowski <marcel2215@icloud.com>
2024-09-27 17:45:18 +0200

    Change preview website links

--- preview/Program.cs
+++ preview/Program.cs
@@ -2,10 +2,10 @@
 
 var scraper = new WebsiteScraper();
 
-await foreach (var link in scraper.ScrapeLinksAsync("https://smmlegal.pl", 2))
+await foreach (var link in scraper.ScrapeLinksAsync("https://chataize.com", 2))
 {
     Console.WriteLine(link);
 }
 
-var homePageContent = await scraper.ScrapeContentAsync("https://smmlegal.pl/en/transformations/6282/");
+var homePageContent = await scraper.ScrapeContentAsync("https://chataize.com");
 Console.WriteLine(homePageContent);
